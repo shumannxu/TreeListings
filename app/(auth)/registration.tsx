af4663c9ed94d2c5a7008c2452e308f6434960da
shumannxu <xu232002@gmail.com>
2024-02-03 23:25:40 -0800

    Add Home, Registration, and Login Pages with Firebase Authentication

---
+++ app/(auth)/registration.tsx
@@ -0,0 +1,192 @@
+import React, { useState } from "react";
+import {
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { router } from "expo-router";
+import { auth, firestore } from "../../firebaseConfig";
+import {
+  sendEmailVerification,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { collection, doc, setDoc } from "firebase/firestore";
+
+export default function RegistrationScreen() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onFooterLinkPress = () => {
+    router.push("/login");
+  };
+
+  const onRegisterPress = () => {
+    if (password !== confirmPassword) {
+      alert("Passwords don't match.");
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((response) => {
+        // Signed in
+        const user = response.user;
+        const uid = response.user.uid;
+        const data = {
+          id: uid,
+          email,
+          fullName,
+        };
+        const usersRef = collection(firestore, "users");
+        const userDoc = doc(usersRef, uid);
+        setDoc(userDoc, data)
+          .then(() => {
+            router.push("/home");
+          })
+          .catch((error) => {
+            alert(error.message);
+          });
+        sendEmailVerification(user)
+          .then((response) => {
+            // Email verification sent
+            // Save the email locally so you don't need to ask the user for it again
+            // if they open the link on the same device.
+            AsyncStorage.setItem("emailForSignIn", email);
+            // Inform the user that the verification email has been sent
+          })
+          .catch((error) => {
+            // Error occurred in sending email verification
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // Handle errors here
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // Handle errors here
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, width: "100%" }}
+        keyboardShouldPersistTaps="always"
+      >
+        <Image style={styles.logo} source={require("../../assets/icon.png")} />
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setFullName(text)}
+          value={fullName}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Confirm Password"
+          onChangeText={(text) => setConfirmPassword(text)}
+          value={confirmPassword}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => onRegisterPress()}
+        >
+          <Text style={styles.buttonTitle}>Create account</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Already got an account?{" "}
+            <Text onPress={onFooterLinkPress} style={styles.footerLink}>
+              Log in
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 120,
+    width: 90,
+    alignSelf: "center",
+    margin: 30,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#788eec",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#788eec",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+
+export default function AppLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    />
+  );
+}
